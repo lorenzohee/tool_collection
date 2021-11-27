7d1d760da28bee1a3ac4ed613e769ff2b7b5f96f
lorenzo <yongqing.he@geely.com>
2021-11-27 11:06:28 +0800

    add spider url

---
+++ spider/model/ofweekurl.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const UrlSchema = new mongoose.Schema(
+  {
+    path: String,
+    created_at: String,
+    read: Boolean,
+    from: String, //来自哪个网站
+  },
+  {
+    versionKey: false,
+  }
+);
+
+module.exports = mongoose.model('OfWeekUrl', UrlSchema);
